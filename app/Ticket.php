7e052a9a0d55b6765a08c970974eb216c5b5ab3e
Jitender S. Padda <jitenderpadda@gmail.com>
2017-08-11 14:23:36 +1000

    Home.blade

--- app/Ticket.php
+++ app/Ticket.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
-    //
+    protected $fillable = [
+        'service_area', 'subject', 'description', 'status', 'user_email'
+    ];
 }

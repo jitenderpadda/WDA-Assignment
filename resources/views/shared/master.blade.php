6a51b092b81bdb403fbc7cd30a012dd9a993627f
Jitender S. Padda <jitenderpadda@gmail.com>
2017-08-15 21:47:33 +1000

    Basic Layout

--- resources/views/shared/master.blade.php
+++ resources/views/shared/master.blade.php
@@ -1,6 +1,7 @@
 <html>
 <head>
     <title> @yield('title') </title>
+    <link rel="shortcut icon" href="{{{ asset('favicon.png') }}}">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
     <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>

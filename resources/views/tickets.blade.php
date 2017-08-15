b313921d8ef074fc78ca360bbea17eea84ec4afc
Jitender S. Padda <jitenderpadda@gmail.com>
2017-08-15 21:00:12 +1000

    View Tickets

--- resources/views/tickets.blade.php
+++ resources/views/tickets.blade.php
@@ -2,6 +2,29 @@
 @extends('shared.master')
 @section('title', 'ITS')
 @section('content')
-    <p> Tickets will be shown here</p>
+    <div class="table-responsive">
+        <table class="table">
+            <thead>
+            <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Status</th>
+                <th>Comment</th>
+            </tr>
+            </thead>
+            <tbody>
 
-    @endsection
+            @foreach ($tickets as $ticket)
+                <tr>
+                    <td>{{$ticket->id}}</td>
+                    <td>{{$ticket->name}}</td>
+                    <td>{{$ticket->email}}</td>
+                    <td>{{$ticket->status}}</td>
+                    <td>{{$ticket->comment}}</td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection

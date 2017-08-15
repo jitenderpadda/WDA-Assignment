0e677b6938617c6c866a7ed2936f2fd68a2b9874
Jitender S. Padda <jitenderpadda@gmail.com>
2017-08-15 18:05:16 +1000

    J - Update

--- app/Http/Controllers/RaiseTicketController.php
+++ app/Http/Controllers/RaiseTicketController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Session;
+use App\Ticket;
 
 class RaiseTicketController extends Controller
 {
@@ -16,6 +17,9 @@ class RaiseTicketController extends Controller
     }
     public function store(RaiseTicketFormRequest $request)
     {
+        $ticket=$request->all();
+        $ticket['status']='pending';
+        Ticket::create($ticket);
         return view('trackProgress');
     }
 }
